31dd85a984f0c7f4a25d731443bf730ef3f965e7
Alfred <alfred.garcia@ucb.edu.bo>
2022-10-03 00:53:10 -0400

    Deteccion de codigo lleno

--- src/Vaca_Toro.spec.js
+++ src/Vaca_Toro.spec.js
@@ -1,10 +1,20 @@
+import codigoSecreto from "./vaca_Toro.js"
+
 describe("CODIGO SECRETO", () => {
-  it("Cuando jugador A ingrese un codigo secreto vacio deberia devolver codigo vacio ", () => {
+  //Alfred
+  it("Cuando jugador A ingrese un codigo secreto vacio, deberia devolver codigo vacio ", () => {
     expect(codigoSecreto("")).toEqual("Codigo Vacio");
   });
-});
-
-function codigoSecreto(codigo) {
+  it("Cuando jugador A ingrese un codigo secreto de 4 digitos, deberia devolver dicho codigo ", () => {
+    expect(codigoSecreto(1206)).toEqual(1206);
+  });
+  it("Cuando jugador A ingrese un codigo secreto de 5 digitos, deberia devolver dicho codigo ", () => {
+    expect(codigoSecreto(45782)).toEqual(45782);
+  });
+  it("Cuando jugador A ingrese un codigo secreto de 6 digitos, deberia devolver dicho codigo ", () => {
+    expect(codigoSecreto(123456)).toEqual(123456);
+  });
   
-  return "Codigo Vacio";
-}
+  //Marcelo
+
+});
