b366c5862daf18a7ce2fc04c9b36ff95def20f34
Alfred <alfred.garcia@ucb.edu.bo>
2022-10-03 00:45:41 -0400

    Deteccion de codigo vacio

---
+++ src/Vaca_Toro.spec.js
@@ -0,0 +1,10 @@
+describe("CODIGO SECRETO", () => {
+  it("Cuando jugador A ingrese un codigo secreto vacio deberia devolver codigo vacio ", () => {
+    expect(codigoSecreto("")).toEqual("Codigo Vacio");
+  });
+});
+
+function codigoSecreto(codigo) {
+  
+  return "Codigo Vacio";
+}
