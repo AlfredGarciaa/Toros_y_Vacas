f078318bf8cf3b458f2c5b3c5e3a62a253505d82
Alfred <alfred.garcia@ucb.edu.bo>
2022-10-04 12:41:18 -0400

    Separar codigo a vector generalizado

--- src/Vaca_Toro.spec.js
+++ src/Vaca_Toro.spec.js
@@ -25,7 +25,14 @@ describe("'JUGADOR B'", () => {
     expect(recibirCodigoB("")).toEqual("Ingresaste codigo vacio");
   });
 // Alfred Garcia
-  it("si jugador B ingresa el numero 5786 este debe devolver el numero en vector [5,7,8,6]", () => {    
+});
+
+describe("'AMBOS JUGADORES'", () => {
+// Alfred Garcia
+  it("si jugador ingresa el numero 5786 este debe devolver el numero en vector [5,7,8,6]", () => {    
     expect(separarCodigo(5786)).toEqual([5,7,8,6]);
+  });
+  it("si jugador ingresa numero 457896 este debe devolver el numero en vector [4,5,7,8,9,6]", () => {    
+    expect(separarCodigo(457896)).toEqual([4,5,7,8,9,6]);
   });
 });
