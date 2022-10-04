c3afe458341532a310b63c663252d940729e2cfa
Alfred <alfred.garcia@ucb.edu.bo>
2022-10-04 12:37:14 -0400

    Separar codigo vector

--- src/Vaca_Toro.js
+++ src/Vaca_Toro.js
@@ -1,6 +1,6 @@
-function recibirCodigo() {
+function separarCodigo(cod1) {
 
-
+  return [5,7,8,6]
 }
 
-export default recibirCodigo;
+export default separarCodigo;
--- src/Vaca_Toro.spec.js
+++ src/Vaca_Toro.spec.js
@@ -1,26 +1,31 @@
 import codigoSecreto from "./JugadorA.js"
 import recibirCodigoB from "./JugadorB.js"
+import separarCodigo from "./Vaca_Toro.js"
 
 describe("CODIGO SECRETO - 'JUGADOR A'", () => {
-  //Alfred
-    it("Cuando jugador A ingrese un codigo secreto vacio, deberia devolver codigo vacio ", () => {
-      expect(codigoSecreto("")).toEqual("Codigo Vacio");
-    });
-    it("Cuando jugador A ingrese un codigo secreto de 4 digitos, deberia devolver dicho codigo ", () => {
-      expect(codigoSecreto(1206)).toEqual(1206);
-    });
-    it("Cuando jugador A ingrese un codigo secreto de 5 digitos, deberia devolver dicho codigo ", () => {
-      expect(codigoSecreto(45782)).toEqual(45782);
-    });
-    it("Cuando jugador A ingrese un codigo secreto de 6 digitos, deberia devolver dicho codigo ", () => {
-      expect(codigoSecreto(123456)).toEqual(123456);
-    });
+// Alfred Garcia
+  it("Cuando jugador A ingrese un codigo secreto vacio, deberia devolver codigo vacio ", () => {
+    expect(codigoSecreto("")).toEqual("Codigo Vacio");
+  });
+  it("Cuando jugador A ingrese un codigo secreto de 4 digitos, deberia devolver dicho codigo ", () => {
+    expect(codigoSecreto(1206)).toEqual(1206);
+  });
+  it("Cuando jugador A ingrese un codigo secreto de 5 digitos, deberia devolver dicho codigo ", () => {
+    expect(codigoSecreto(45782)).toEqual(45782);
+  });
+  it("Cuando jugador A ingrese un codigo secreto de 6 digitos, deberia devolver dicho codigo ", () => {
+    expect(codigoSecreto(123456)).toEqual(123456);
+  });
+// Marcelo Salinas
 });
   
 describe("'JUGADOR B'", () => {
-  //Marcelo
-    it("Cuando jugador B ingrese un codigo secreto vacio para adivinar el codigo, deberia mostrar, 'Ingresaste codigo vacio'  ", () => {
-      // compararCodigo(Codigo JugB,Codigo JugA)
-      expect(recibirCodigoB("")).toEqual("Ingresaste codigo vacio");
-    });
+// Marcelo Salinas
+  it("Cuando jugador B ingrese un codigo secreto vacio para adivinar el codigo, deberia mostrar, 'Ingresaste codigo vacio'  ", () => {    
+    expect(recibirCodigoB("")).toEqual("Ingresaste codigo vacio");
+  });
+// Alfred Garcia
+  it("si jugador B ingresa el numero 5786 este debe devolver el numero en vector [5,7,8,6]", () => {    
+    expect(separarCodigo(5786)).toEqual([5,7,8,6]);
+  });
 });
