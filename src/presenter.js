7ae1880b1e41ee81df37554eba54f01d56cc2177
chelo7717 <chelo336@gmail.com>
2022-10-06 09:38:07 -0400

    implemtando html

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,16 @@
-//import sumar from "./Vaca_Toro.js";
+import Separar_Codigo from "./Separador.js";
+import Buscar_Vaca_Toro from "./Vaca_Toro.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const first = document.querySelector("#codigo-secreto");
+const second = document.querySelector("#codigo-adiv");
+const form = document.querySelector("#JUEGO-VACASYTOROS-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const codsecr = Separar_Codigo(first.value);
+  const codadiv = Separar_Codigo(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + Buscar_Vaca_Toro(codsecr, codadiv) + "</p>";
 });
