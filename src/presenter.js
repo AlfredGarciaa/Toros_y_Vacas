84c1afbfaa467bd3697a1feb4ff560f09751a226
Alfred <alfred.garcia@ucb.edu.bo>
2022-10-08 18:46:16 -0400

    Respuesta de HTML

--- src/presenter.js
+++ src/presenter.js
@@ -1,19 +1,23 @@
 import Vaca_Toro from "./Vaca_Toro.js";
 
-const codigoSecreto = document.querySelector("codigo-secreto");
+const codigoSecreto = document.querySelector("#codigo-secreto");
 const intent = document.querySelector("#intento");
 const form = document.querySelector("#inicio-form");
 const form2 = document.querySelector("#intento-form");
 const div = document.querySelector("#resultado-div");
 
+let aux;
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  div.innerHTML = "<p>" + "HOLA" + "</p>";
+  div.innerHTML = "<p>" + aux + "</p>";
 });
 
 form2.addEventListener("submit", (event) => {
   event.preventDefault();
+  const firstNumber = Number.parseInt(intent.value);
   let juego = new Vaca_Toro();
-  let resultado = juego.Comparar_Codigo(codigoSecreto,intent)
+  aux = Number.parseInt(codigoSecreto.value);
+  let resultado = juego.Comparar_Codigo(aux,firstNumber);
   div.innerHTML = "<p>" + resultado + "</p>";
 });
