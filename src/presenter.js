a837bfd2667a75e26152e1038272b3b740cabd0c
Alfred <alfred.garcia@ucb.edu.bo>
2022-10-08 18:49:57 -0400

    Interfaz VERSION 1

--- src/presenter.js
+++ src/presenter.js
@@ -1,23 +1,32 @@
 import Vaca_Toro from "./Vaca_Toro.js";
 
+const numIntentos = document.querySelector("#intentos");
 const codigoSecreto = document.querySelector("#codigo-secreto");
 const intent = document.querySelector("#intento");
 const form = document.querySelector("#inicio-form");
 const form2 = document.querySelector("#intento-form");
 const div = document.querySelector("#resultado-div");
 
-let aux;
+let secreto;
+let intentos;
 
 form.addEventListener("submit", (event) => {
+
   event.preventDefault();
-  div.innerHTML = "<p>" + aux + "</p>";
+  secreto = Number.parseInt(codigoSecreto.value);
+  intentos = Number.parseInt(numIntentos.value);
+  form.reset();
 });
 
 form2.addEventListener("submit", (event) => {
+  
   event.preventDefault();
   const firstNumber = Number.parseInt(intent.value);
   let juego = new Vaca_Toro();
-  aux = Number.parseInt(codigoSecreto.value);
-  let resultado = juego.Comparar_Codigo(aux,firstNumber);
-  div.innerHTML = "<p>" + resultado + "</p>";
+  let resultado = juego.Comparar_Codigo(secreto,firstNumber);
+
+  div.innerHTML = "<p>" + resultado + " Te quedan: " + intentos-- + " intentos" + "</p>";
+  if(intentos==0) {
+    div.innerHTML = "<p>" + "Perdiste el juego por falta de intentos" +" El codigo secreto era: "+ secreto +"</p>";
+  }
 });
