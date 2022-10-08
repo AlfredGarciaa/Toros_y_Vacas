b7658c762b011ad7ef042bf5ec4fdb8a81404b39
Alfred <alfred.garcia@ucb.edu.bo>
2022-10-08 18:44:32 -0400

    Presenter con adivinar

--- src/presenter.js
+++ src/presenter.js
@@ -1,11 +1,19 @@
-//import Vaca_Toro from "./Vaca_Toro.js";
+import Vaca_Toro from "./Vaca_Toro.js";
 
 const codigoSecreto = document.querySelector("codigo-secreto");
 const intent = document.querySelector("#intento");
 const form = document.querySelector("#inicio-form");
+const form2 = document.querySelector("#intento-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   div.innerHTML = "<p>" + "HOLA" + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let juego = new Vaca_Toro();
+  let resultado = juego.Comparar_Codigo(codigoSecreto,intent)
+  div.innerHTML = "<p>" + resultado + "</p>";
 });
